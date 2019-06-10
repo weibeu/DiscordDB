ded9a6a7c90864e6fb92f091bfd97772526adfc6
thecosmos <deepakrajko14@gmail.com>
2019-06-10 09:27:48 +0530

    Conf changes

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
-master_doc = 'index.rst'
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
